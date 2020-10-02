435f1e9c82ef4bdde4d8e1e2ef5e2506658cc5c9
Sagar Makhija <sagar1309@live.com>
2020-10-02 17:20:31 +0530

    refactor app.js
    - separate file to make api calls
    - env file handles sensitive variables and not app.js

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const logger = require('morgan');
 const cors = require('cors');
 const axios = require('axios');
 
+const api = require('./api');
+
 // Configuration
-const PORT = 3000;
+const { baseUrl } = require('./config');
+const { port } = require('./config');
 const HOST = "localhost";
-const API_SERVICE_URL = "https://public-api.wordpress.com/rest/v1.1/sites/107403796";
 
 // logs out whatever happens
 app.use(logger('dev'));
@@ -16,7 +18,7 @@ app.use(cors());
 app.use('/posts/:postID/related', async function (req, res, next) {
   try {
     const postID = req && req.params && req.params.postID;
-    const response = await axios.post(`${API_SERVICE_URL}/posts/${postID}/related`);
+    const response = await api.relatedBlogPostById({ id: postID });
     data = response.data;
     res.status(200).json(data);
   } catch (error) {
@@ -30,7 +32,7 @@ app.use('/posts/:postID/related', async function (req, res, next) {
 app.use('/posts/:postID', async function (req, res, next) {
   try {
     const postID = req && req.params && req.params.postID;
-    const response = await axios.get(`${API_SERVICE_URL}/posts/${postID}`);
+    const response = await api.postById({ id: postID });
     data = response.data;
     res.send(data);
   } catch (error) {
@@ -45,28 +47,13 @@ app.use('/posts', async function (req, res, next) {
   // /posts                            - all posts-----------------DONE
   // /posts?number=25&offset=0&page=1  - top 25 posts--------------DONE
   // /posts/?number=4&offset=25        - next 4 posts--------------DONE
-  // /posts/7977                       - single post
-  // /posts/7977/related               - related posts
+  // /posts/7977                       - single post---------------DONE
+  // /posts/7977/related               - related posts-------------DONE
   // /posts?category=tech              - posts of a category-------DONE
   // /posts?tag=iphone                 - posts of a tag------------DONE
   try {
-
     const query = req.query;
-    const numberQuery = query.number || null;
-    const offsetQuery = query.offset || null;
-    const pageQuery = query.page || null;
-    const categoryQuery = query.category || null;
-    const tagQuery = query.tag || null;
-
-    const response = await axios.get(`${API_SERVICE_URL}/posts`, {
-      params: {
-        number: numberQuery,
-        offset: offsetQuery,
-        page: pageQuery,
-        category: categoryQuery,
-        tag: tagQuery
-      }
-    });
+    const response = await api.posts(query);
     data = response.data;
     res.status(200).json(data);
   } catch (error) {
@@ -79,10 +66,13 @@ app.use('/posts', async function (req, res, next) {
 
 app.use('/categories', async function (req, res, next) {
   try {
-    const response = await axios.get(`${API_SERVICE_URL}/categories`);
+    // const response = await axios.get(`${baseUrl}/categories`);
+    const response = await api.categories();
+    console.log("response", response);
     data = response.data;
     res.status(200).json(data);
   } catch (error) {
+    console.log("error", error);
     const errorResponse = error && error.response && error.response;
     const errorStatus = errorResponse.status;
     const errorData = errorResponse.data;
@@ -92,7 +82,7 @@ app.use('/categories', async function (req, res, next) {
 
 app.use('/tags', async function (req, res, next) {
   try {
-    const response = await axios.get(`${API_SERVICE_URL}/tags`);
+    const response = await api.tags();
     data = response.data;
     res.status(200).json(data);
   } catch (error) {
@@ -122,7 +112,7 @@ app.use('/tags', async function (req, res, next) {
 
 //   try {
 //     let data;
-//     let url = postID ? `${API_SERVICE_URL}/${paramType}/${postID}` : `${API_SERVICE_URL}/${paramType}`;
+//     let url = postID ? `${baseUrl}/${paramType}/${postID}` : `${baseUrl}/${paramType}`;
 //     const response = await axios.get(`${url}`, {
 //       params: {
 //         number: numberQuery,
@@ -139,8 +129,8 @@ app.use('/tags', async function (req, res, next) {
 //   }
 // });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+app.listen(port, HOST, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
 module.exports = app;
