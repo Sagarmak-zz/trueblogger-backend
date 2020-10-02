57b838abfcf79a3dffb167373ad28d38714f10fb
Sagar Makhija <sagar1309@live.com>
2020-10-02 21:30:51 +0530

    rename .env and remove unused code in app.js

--- app.js
+++ app.js
@@ -46,13 +46,6 @@ app.use('/posts/:postID', async function (req, res, next) {
 });
 
 app.use('/posts', async function (req, res, next) {
-  // /posts                            - all posts-----------------DONE
-  // /posts?number=25&offset=0&page=1  - top 25 posts--------------DONE
-  // /posts/?number=4&offset=25        - next 4 posts--------------DONE
-  // /posts/7977                       - single post---------------DONE
-  // /posts/7977/related               - related posts-------------DONE
-  // /posts?category=tech              - posts of a category-------DONE
-  // /posts?tag=iphone                 - posts of a tag------------DONE
   try {
     const query = req.query;
     const response = await api.posts(query);
@@ -94,42 +87,6 @@ app.use('/tags', async function (req, res, next) {
     res.status(errorStatus).send(errorData);
   }
 });
-
-// app.use('/:type', async function (req, res, next) {
-
-//   /**
-//    * Param Type can be a
-//    * Post, Tag, Category
-//    */
-//   const paramType = req && req.params && req.params.type;
-
-//   const query = req.query;
-//   console.log("query", query);
-//   const numberQuery = query.number || null;
-//   const offsetQuery = query.offset || null;
-//   const pageQuery = query.page || null;
-//   const categoryQuery = query.category || null;
-//   const tagQuery = query.tag || null;
-//   const postID = query.postID || null;
-
-//   try {
-//     let data;
-//     let url = postID ? `${baseUrl}/${paramType}/${postID}` : `${baseUrl}/${paramType}`;
-//     const response = await axios.get(`${url}`, {
-//       params: {
-//         number: numberQuery,
-//         offset: offsetQuery,
-//         page: pageQuery,
-//         category: categoryQuery,
-//         tag: tagQuery
-//       }
-//     });
-//     data = response.data;
-//     res.status(200).json(data);
-//   } catch (error) {
-//     console.log('err', error);
-//   }
-// });
 
 app.listen(port, HOST, () => {
   console.log(`Example app listening at http://localhost:${port}`);
